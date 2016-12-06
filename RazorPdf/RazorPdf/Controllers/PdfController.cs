574763cb5417f73a3a143ef44942b69868a0f849
xxxmatko <martin.sutka@gmail.com>
2016-12-06 09:34:56 +0100

    Default view. Added Roboto font.

--- RazorPdf/RazorPdf/Controllers/PdfController.cs
+++ RazorPdf/RazorPdf/Controllers/PdfController.cs
@@ -19,15 +19,51 @@ namespace RazorPdf.Controllers
         {
             var model = new List<Employee>()
                         {
-                            new Employee() { Id = 1, Name = "Dominic Galloway", Email = "justo@malesuadaIntegerid.edu" },
-                            new Employee() { Id = 2, Name = "Austin Hopper", Email = "pede.et.risus@vehicula.ca" },
-                            new Employee() { Id = 3, Name = "Amery Carroll", Email = "tincidunt.pede@non.co.uk" },
-                            new Employee() { Id = 4, Name = "Owen Holder", Email = "eu@dapibusgravidaAliquam.co.uk" },
-                            new Employee() { Id = 5, Name = "Jarrod Oneil", Email = "sodales@congueIn.net" },
-                            new Employee() { Id = 6, Name = "Tarik Newman", Email = "metus@necluctusfelis.ca" },
-                            new Employee() { Id = 7, Name = "Xenos Odonnell", Email = "magna.a@sapienmolestieorci.org" },
-                            new Employee() { Id = 8, Name = "Carlos David", Email = "Praesent.interdum.ligula@anteiaculisnec.co.uk" },
-                            new Employee() { Id = 9, Name = "Walter Gamble", Email = "rhoncus.id.mollis@adipiscingfringillaporttitor.org" }
+                            new Employee()
+                            {
+                                Id = 1, Name = "Dominic Galloway", 
+                                Email = "justo@malesuadaIntegerid.edu"
+                            },
+                            new Employee()
+                            {
+                                Id = 2, Name = "Austin Hopper", 
+                                Email = "pede.et.risus@vehicula.ca"
+                            },
+                            new Employee()
+                            {
+                                Id = 3, Name = "Amery Carroll", 
+                                Email = "tincidunt.pede@non.co.uk"
+                            },
+                            new Employee()
+                            {
+                                Id = 4, Name = "Owen Holder", 
+                                Email = "eu@dapibusgravidaAliquam.co.uk"
+                            },
+                            new Employee()
+                            {
+                                Id = 5, Name = "Jarrod Oneil", 
+                                Email = "sodales@congueIn.net"
+                            },
+                            new Employee()
+                            {
+                                Id = 6, Name = "Tarik Newman", 
+                                Email = "metus@necluctusfelis.ca"
+                            },
+                            new Employee()
+                            {
+                                Id = 7, Name = "Xenos Odonnell", 
+                                Email = "magna.a@sapienmolestieorci.org"
+                            },
+                            new Employee()
+                            {
+                                Id = 8, Name = "Carlos David", 
+                                Email = "Praesent.interdum.ligula@anteiaculisnec.co.uk"
+                            },
+                            new Employee()
+                            {
+                                Id = 9, Name = "Walter Gamble", 
+                                Email = "rhoncus.id.mollis@adipiscingfringillaporttitor.org"
+                            }
                         };
             
             return View(model);
